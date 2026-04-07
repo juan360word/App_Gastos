12bd76b1af52278073b42166f0dedf4985e934e8
Juan Serrato <serratoalvaradojuandavid@gmail.com>
2026-04-06 22:55:43 -0500

    V2

--- src/Componentes/Form.tsx
+++ src/Componentes/Form.tsx
@@ -1,8 +1,7 @@
 
 // Importaciones
 
-import { useState } from "react"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { usePrupuesto } from "../Hook/usePresupuesto"
 
 
@@ -10,7 +9,7 @@ function Form() {
     const [Presupuesto, SetPresupuesto ] = useState(0)
     const {dispatch} = usePrupuesto()
 
-    const HandleChase = (e : React.ChangeEvent<HTMLInputElement, HTMLInputElement>)  => {
+    const HandleChase = (e: React.ChangeEvent<HTMLInputElement>)  => {
         const Decimal = e.target.value.replace(/\D/g, "")
         SetPresupuesto(e.target.valueAsNumber),
         SetPresupuesto(Number(Decimal))
@@ -20,15 +19,15 @@ function Form() {
              return  isNaN(Presupuesto) || Presupuesto <= 0 
             },[Presupuesto]) 
 
-    const HandleSubmit = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+    const HandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        console.log('MUESTRA de que sirve')
         dispatch({type: 'Agregar' ,payload :{Presupuesto}})
     }
 
   return (
     <>
-    <form action="" className="space-y-5">
+    <form action="" className="space-y-5" onSubmit={HandleSubmit}>
         <div className="flex flex-col space-y-5 ">
             <label htmlFor="Gurned" className="text-center text-4xl text-white tracking-widest font-bold ">Definicion Del Presupuesto</label>
             <input type="number" className="max-w-5xl p-2 m-5 border rounded-xl  border-white bg-white mx-auto" 
@@ -38,7 +37,7 @@ function Form() {
                 onChange={HandleChase}
             />
         </div>
-        <input type="submit" onChange={HandleSubmit} disabled={Validacion} value={'Definicion Del Dinero'} className="w-full border bg-white p-2 tracking-widest font-black cursor-pointer disabled:opacity-30  " />
+        <input type="submit" disabled={Validacion} value={'Definicion Del Dinero'} className="w-full border bg-white p-2 tracking-widest font-black cursor-pointer disabled:opacity-30  " />
     </form>
     </>
   )
